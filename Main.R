0c2e92a63429d52e93922867ef15441b3cde0df2
GranDurismo <rxaviermontero@gmail.com>
2019-01-29 00:42:35 -0300

    Move date and type outside of sapply

--- Main.R
+++ Main.R
@@ -34,20 +34,18 @@ ext <- ext[grep("[a-z0-9-]{12,16}",ext)]
 # Loop to build final monthly URLs and scrape each of them for the data
 norm <- sapply(ext, function(x) {tryCatch({
   url <- paste0(baseurl, x)
-  type <- str_extract(url,"/[a-z]{5,8}/") %>% {gsub("/","",.)} %>% str_to_title()
   web <- read_html(url)
   text <- html_nodes(web, "#desarrollo a") %>% html_text() %>% trimws() %>% {gsub("\n          "," ",.)}
   cant <- length(text) %>% as.numeric()
-  date <- str_extract(url,"-[0-9]+-[0-9]{2,4}") %>% substring(2)
-  list(url,date,type,cant,text)},
-  error=function(e) c(url,date,type,NA,NA))
+  list(url,cant,text)},
+  error=function(e) c(url,NA,NA))
 })
 
 # Build the dataframe, parse dates and set classes
-df<-cbind(norm[1,],norm[2,],norm[3,],norm[4,]) %>% data.frame()
-colnames(df) <- c("URL","Date","Type","Count")
-df$Date <- parse_date_time(df$Date,orders=c("mY","my")) %>% as.Date()
+df<-cbind(norm[1,],norm[2,]) %>% data.frame()
+colnames(df) <- c("URL","Count")
+df$Date <- str_extract(df$URL,"-[0-9]+-[0-9]{2,4}") %>% substring(2) %>% parse_date_time(orders=c("mY","my")) %>% as.Date()
+df$Type <- str_extract(df$URL,"/[a-z]{5,8}/") %>% {gsub("/","",.)} %>% str_to_title()
 df$Count <- as.numeric(df$Count)
 df$URL <- as.character(df$URL)
-df$Type <- as.character(df$Type)
 beep()
