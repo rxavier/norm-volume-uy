35ce6ca6cd1b1b2fde1dfacab47fe9e9e8ce005b
GranDurismo <rxaviermontero@gmail.com>
2019-01-28 23:44:04 -0300

    Account for 2017-19 URLs which have a use years without centuries

--- Volnormativo.R
+++ Volnormativo.R
@@ -37,7 +37,7 @@ norm <- sapply(ext, function(x) {tryCatch({
   web <- read_html(url)
   text <- html_nodes(web, "#desarrollo a") %>% html_text() %>% trimws() %>% {gsub("\n          "," ",.)}
   cant <- length(text) %>% as.numeric()
-  date <- str_extract(url,"-[0-9]+-[0-9]{4}") %>% substring(2)
+  date <- str_extract(url,"-[0-9]+-[0-9]{2,4}") %>% substring(2)
   list(url,date,type,cant,text)},
   error=function(e) c(url,date,type,NA,NA))
 })
