5fcd46a84c4314f0be5596de9d02a15f11ffa424
GranDurismo <rxaviermontero@gmail.com>
2019-01-30 01:41:15 -0300

    Placeholder. Randomly check 10 strings matching the most used words

--- Words.R
+++ Words.R
@@ -6,3 +6,5 @@ wdf <- names(wordfreq) %>% str_remove_all("\\s+") %>%
 colnames(wdf) <- c("A","B")
 wdf <- subset(wdf,str_length(as.character(wdf$A))>3)
 wdf <- wdf[order(-wdf$B),]
+
+#sapply(as.character(wdf[1:2,1]), function(x) sample(str_subset(unlist(norm[3,]),"\bx\b"),10))
