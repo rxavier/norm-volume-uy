9843896102c1065be7c8cca51af48a4d22e84db2
GranDurismo <rxaviermontero@gmail.com>
2019-02-04 22:55:57 -0300

    Create regex vector and sample norms matching the most frequent strings

--- Words.R
+++ Words.R
@@ -2,9 +2,10 @@ load("./Data.RData")
 
 wordfreq <- str_split(unlist(norm[3,])," ") %>% {table(unlist(.))}
 wdf <- names(wordfreq) %>% str_remove_all("\\s+") %>%
-  cbind.data.frame(as.integer(wordfreq)) 
+  cbind.data.frame(as.integer(wordfreq),stringsAsFactors=F) 
 colnames(wdf) <- c("A","B")
 wdf <- subset(wdf,str_length(as.character(wdf$A))>3)
 wdf <- wdf[order(-wdf$B),]
 
-#sapply(as.character(wdf[1:2,1]), function(x) sample(str_subset(unlist(norm[3,]),"\bx\b"),10))
+samples <- unlist(wdf[1:20,1]) %>% paste0("\\b",.,"\\b") %>% 
+  sapply(function(x) str_subset(unlist(norm[3,]),x) %>% sample(5))
