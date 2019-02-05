9b267100c83c570126061f5c0cdb62a7357680d1
GranDurismo <rxaviermontero@gmail.com>
2019-02-05 00:17:01 -0300

    Get samples from words with frequency > 99

--- Words.R
+++ Words.R
@@ -7,6 +7,7 @@ colnames(wdf) <- c("A","B")
 wdf <- subset(wdf,str_length(as.character(wdf$A))>3)
 wdf <- wdf[order(-wdf$B),]
 
-samples <- unlist(wdf[1:20,1]) %>% paste0("\\b",.,"\\b") %>% 
-  sapply(function(x) str_subset(unlist(norm[3,]),x) %>% sample(5))
+## Get the words that are repeated more than 100 times and sample 10 norms that match each
+samples <- sum(wdf$B>99) %>% {unlist(wdf[1:.,1])} %>% paste0("\\b",.,"\\b") %>% 
+  sapply(function(x) str_subset(unlist(norm[3,]),x) %>% sample(10))
 colnames(samples) <- wdf[1:ncol(samples),1]
