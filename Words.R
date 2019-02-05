c207ce4e922e89418faa7054f0a8a240d5e1e1e7
GranDurismo <rxaviermontero@gmail.com>
2019-02-05 01:00:32 -0300

    Tentative word exclusions

--- Words.R
+++ Words.R
@@ -13,3 +13,9 @@ wdf <- wdf[order(-wdf$B),]
 samples <- sum(wdf$B>99) %>% {unlist(wdf[1:.,1])} %>% paste0("\\b",.,"\\b") %>% 
   sapply(function(x) str_subset(unlist(norm[3,]),x) %>% sample(10))
 colnames(samples) <- wdf[1:ncol(samples),1]
+
+exclude <- c("SUBGRUPO","GRUPO","CONVENIO","ACUERDO","COLECTIVO","UNIDAD REAJUSTABLE",
+             "unidad reajustable","U.R.","UR","U.R.A.","URA","Se fija","Se actualiza","SUSCRITO",
+             "ANEXO","DESIGNA", "DESIGNACIÓN","ESCUELA","PARTIDAS","COMISIÓN", "MERCOSUR",
+             "MERCADO COMÚN","EMISIÓN","SALARIO MÍNIMO NACIONAL",
+             "MONTO MÍNIMO DE LAS JUBILACIONES","INTERÉS NACIONAL", "COMPLEMENTACIÓN","COOPERACIÓN")
