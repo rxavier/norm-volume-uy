5db80b6e218d260b0fb2bdd48c556e31eb31f73a
GranDurismo <rxaviermontero@gmail.com>
2019-03-19 00:56:33 -0300

    Add column specifying law or decree

--- IMPO.R
+++ IMPO.R
@@ -26,8 +26,8 @@ headers <- c("Accept"="text/html,application/xhtml+xml,application/xml;q=0.9,ima
              "User-Agent"="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36")
 
 # Create start and end dates to be used in each function call
-date_pub_start <- "01-03-1985" %>% as.Date("%d-%m-%Y")
-date_pub_end <- "01-02-2019" %>% as.Date("%d-%m-%Y")
+date_pub_start <- "01-12-2018" %>% as.Date("%d-%m-%Y")
+date_pub_end <- "01-01-2019" %>% as.Date("%d-%m-%Y")
 date_df <- as.data.frame(as.Date(seq(as.yearmon(date_pub_start), as.yearmon(date_pub_end), by=1/12), frac=1)) %>%
   `colnames<-` ("End")
 date_df$Start <- date_df$End-as.POSIXlt(date_df$End)$mday + 1
@@ -37,10 +37,10 @@ dates_url <- paste0("&fechadiar1=", str_replace_all(format(date_df$Start, "%d-%m
 # Run function for laws and decrees
 type_norm <- type_norm_vec[c("Laws", "Decrees")]
 data <- lapply(type_norm, function(x) request_norm_dates(x, dates_url))
-flat_df_laws <- cbind.data.frame(unlist(data[[1]][1, ]), unlist(data[[1]][2, ]), unlist(data[[1]][3, ])) %>%
-  `colnames<-` (c("Number", "Type", "Text"))
-flat_df_decrees <- cbind.data.frame(unlist(data[[2]][1, ]), unlist(data[[2]][2, ]), unlist(data[[2]][3, ])) %>%
-  `colnames<-` (c("Number", "Type", "Text"))
+flat_df_laws <- cbind.data.frame(unlist(data[[1]][1, ]), unlist(data[[1]][2, ]), unlist(data[[1]][3, ]), "Law") %>%
+  `colnames<-` (c("Number", "Type2", "Text", "Type1"))
+flat_df_decrees <- cbind.data.frame(unlist(data[[2]][1, ]), unlist(data[[2]][2, ]), unlist(data[[2]][3, ]), "Decree") %>%
+  `colnames<-` (c("Number", "Type2", "Text", "Type1"))
 flat_df <- rbind.data.frame(flat_df_laws, flat_df_decrees)
 flat_df["month"] <- rownames(flat_df) %>% str_extract_all("(?<=fechadiar2=)[0-9%F]+") %>%
   str_replace_all("%2F", "-") %>% as.Date("%d-%m-%Y")
