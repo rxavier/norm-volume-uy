7f03d58d2e26c3ac7cbc142a9886f4ee5da3799f
GranDurismo <rxaviermontero@gmail.com>
2019-03-19 00:36:27 -0300

    Rename function arg

--- Request_and_parse.R
+++ Request_and_parse.R
@@ -1,15 +1,15 @@
-request_norm_dates <- function(type_norm, date_list) {
-  if (type_norm==5) {
+request_norm_dates <- function(type, date_list) {
+  if (type==5) {
     number_docs <- 50}
-  else if (type_norm==6) {
+  else if (type==6) {
     number_docs <- 100}
-  else if (type_norm==7) {
+  else if (type==7) {
     number_docs <- 150}
   else {number_docs <- 10}
   
   sapply(date_list, function(x) {
     month <- str_extract_all(x, "(?<=fechadiar2=)[0-9%F]+") %>% str_replace_all("%2F", "-") %>% as.Date("%d-%m-%Y")
-    url <- paste0(impo_url, suffix0, number_docs, "&combo1=", type_norm, suffix1, x, suffix2)
+    url <- paste0(impo_url, suffix0, number_docs, "&combo1=", type, suffix1, x, suffix2)
     request_html <- GET(url, add_headers(headers)) %>% read_html()
     check1 <- html_nodes(request_html, ".contenido a") %>% html_text() %>% trimws() %>% toString()
     check2 <- (html_nodes(request_html, "p") %>% html_text() %>% trimws())[2] %>% toString()
@@ -19,14 +19,16 @@ request_norm_dates <- function(type_norm, date_list) {
       request_html <- GET(url, add_headers(headers)) %>% read_html()
       print(paste0(format(month, "%m-%Y"), ": URL refreshed"))}
     else if (check2 == nodoc_msg) {
-      print(paste0(format(month, "%m-%Y"), ": no ", tolower(names(type_norm)), " found"))
+      print(paste0(format(month, "%m-%Y"), ": no ",
+                   tolower(names(type_norm)[which(type_norm==type)]), " found"))
       return(list(NA, NA, NA))}
     
     number_docs_effective <- (html_nodes(request_html, "#divMsg b") %>% html_text() %>% trimws())[1]
-    print(paste0(format(month, "%m-%Y"),": ", number_docs_effective, " ", tolower(names(type_norm)), " found"))
+    print(paste0(format(month, "%m-%Y"),": ", number_docs_effective, " ",
+                 tolower(names(type_norm)[which(type_norm==type)]), " found"))
     
     if (number_docs_effective>=number_docs) {
-      url <- paste0(impo_url, suffix0, number_docs_effective, "&combo1=", type_norm, suffix1, x, suffix2)
+      url <- paste0(impo_url, suffix0, number_docs_effective, "&combo1=", type, suffix1, x, suffix2)
       request_html <- GET(url, add_headers(headers)) %>% read_html()}
     
     norm_number <- html_nodes(request_html, "strong") %>% html_text() %>% trimws()
