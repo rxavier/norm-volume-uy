32ae760514e3be521224babd94d35fe2a0c4adc4
Rafael Xavier <rxaviermontero@gmail.com>
2019-05-14 00:27:17 -0300

    Reformat whole scripts

--- Get_data.R
+++ Get_data.R
@@ -1,59 +1,49 @@
 library(stringr)
 
-# Load function
+# Load functions
 source('Request_and_parse.R')
 
-# Set defaults and initial values
-impo_url <- "https://www.impo.com.uy"
-suffix0 <- "/cgi-bin/bases/consultaBasesBS.cgi?tipoServicio=3&realizarconsulta=SI&nuevaconsulta=SI&parlistabases=&nrodocdesdehasta=0-"
-suffix1 <- "&numeros=&articulos=&textolibre=&texto1=&campotexto1=TODOS&optexto1=Y&texto2=&campotexto2=TODOS&optexto2=Y&texto3=&campotexto3=TODOS"
-suffix2 <- "&fechadiar1=&fechadiar2=&comboBaseTematica=&indexcombobasetematica=0&tema=&nvocabulario=&refinar="
-
-refresh_msg <- "Haga clic aqui para ingresar nuevamente al sistema."
-nodoc_msg <- "Su búsqueda no produjo ningún documento."
-headers <- c("Accept"="text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-             "Accept-Encoding"="gzip, deflate, br",
-             "Accept-Language"="es-UY,es;q=0.9,en-US;q=0.8,en;q=0.7,es-419;q=0.6",
-             "Cache-Control"="max-age=0",
-             "Connection"="keep-alive",
-             "Cookie"="__utma=175831610.746919901.1511621124.1511621124.1511621124.1; usrts_3=anonimo; idsesionanonimo=z7lLlTGP",
-             "DNT"="1",
-             "Host"="www.impo.com.uy",
-             "Upgrade-Insecure-Requests"="1",
-             "User-Agent"="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36")
-
-type_norm_vec <- c("Law"=5, "Decree"=6, "Resolution"=7, "Rule"=11)
-
-# Create start and end dates to be used in each function call
-dates_url <- function(start_date, end_date) {
+# Produce dataframe for specified norm between dates
+df_norms <- function(type, start, end, date_format="%d-%m-%Y", write=FALSE) {
   
-  date_pub_start <- start_date %>% as.Date("%d-%m-%Y")
-  date_pub_end <- end_date %>% as.Date("%d-%m-%Y")
+  dates <- dates_url(start_date=start, end_date=end, date_format=date_format)
   
-  date_df <- as.data.frame(as.Date(seq(as.yearmon(date_pub_start), as.yearmon(date_pub_end), by=1/12), frac=1)) %>%
-    `colnames<-` ("End")
-  date_df$Start <- date_df$End-as.POSIXlt(date_df$End)$mday + 1
+  df_aux <- request_norm_dates(type, dates)
   
-  paste0("&fechapro1=", str_replace_all(format(date_df$Start, "%d-%m-%Y"),"-","%2F"),
-         "&fechapro2=", str_replace_all(format(date_df$End, "%d-%m-%Y"),"-","%2F"))
+  missing <- which(is.na(df_aux$Number), arr.ind=TRUE)
+  missing_dates <- missing %>% {row.names(df_aux[. , ])}
+  
+  if (length(missing) > 0) {
+    df_norms_comp <- retry_request(df_aux, type, missing_dates)
+  } else {df_norms_comp <- df_aux}
+  
+  df_norms_comp["Month"] <- rownames(df_norms_comp) %>% str_extract_all("(?<=fechapro2=)[0-9%F]+") %>%
+    str_replace_all("%2F", "-") %>% as.Date("%d-%m-%Y")
+  df_comp_nodupl <- df_norms_comp[!duplicated(df_norms_comp[, 1]) | !duplicated(df_norms_comp[, 6]), ] %>% 
+  {.[order(.$Type, .$Month), ]} %>% 
+  {.[, c("Month", "Type", "Number", "Title", "Type2", "URL")]} %>%
+  {.[!(.$Title==""), ]} %>% `rownames<-` (NULL)
+  
+  if (write==TRUE) {
+    write.csv(df_comp_nodupl, paste0("Data/", type, "_", start, "_", end, ".csv"), row.names=FALSE)}
+  
+  return(df_comp_nodupl)
 }
 
-# Function for producting dataframes per norm type
-df_norms <- function(type, dates_url) {
-  df_aux <- request_norm_dates(type, dates_url)
-  missing <- which(is.na(df_aux$Number), arr.ind=TRUE)
-  if (length(missing_laws) > 0) {
-    df_norms_comp <- retry_request(df_aux, type, missing)
-  } else {df_norms_comp <- df_aux}
+
+# Update function
+update_request <- function(df, type, end, date_format="%d-%m-%Y", write=FALSE) {
+  
+  start <- df[order(df$Month), ] %>% .[nrow(.), 1] + 1
+  
+  df_update <- df_norms(type, start, end, date_format=date_format, write=FALSE)
+  df_comp_nodupl <- rbind.data.frame(df, df_update, stringsAsFactors=F)
+  
+  if (write==TRUE) {
+    write.csv(df_comp_nodupl, paste0("Data/", type, "_", start, "_", end, ".csv"), row.names=FALSE)}
+  
+  return(df_comp_nodupl)
 }
 
 # Bind laws and decrees dataframes, add dates, drop duplicates and export to .csv
-df_comp <- rbind.data.frame(df_laws_comp, df_decrees_comp, stringsAsFactors=F) %>% 
-  {.[!(.$Title==""), ]}
-df_comp["Month"] <- rownames(df_comp) %>% str_extract_all("(?<=fechapro2=)[0-9%F]+") %>%
-  str_replace_all("%2F", "-") %>% as.Date("%d-%m-%Y")
-df_comp_nodupl <- df_comp[!duplicated(df_comp[, 1]) | !duplicated(df_comp[, 6]), ] %>% 
-{.[order(.$Type, .$Month), ]} %>% 
-  {.[, c("Month", "Type", "Number", "Title", "Type2", "URL")]} %>%`rownames<-` (NULL)
-
-write.csv(df_comp_nodupl, "Data/Data_1985_2018.csv", row.names=FALSE)
+df_comp <- rbind.data.frame(df_laws_comp, df_decrees_comp, stringsAsFactors=F) 
--- Request_and_parse.R
+++ Request_and_parse.R
@@ -1,6 +1,45 @@
 library(httr)
 library(rvest)
 library(zoo)
+
+# Set defaults and initial values
+impo_url <- "https://www.impo.com.uy"
+suffix0 <- "/cgi-bin/bases/consultaBasesBS.cgi?tipoServicio=3&realizarconsulta=SI&nuevaconsulta=SI&parlistabases=&nrodocdesdehasta=0-"
+suffix1 <- "&numeros=&articulos=&textolibre=&texto1=&campotexto1=TODOS&optexto1=Y&texto2=&campotexto2=TODOS&optexto2=Y&texto3=&campotexto3=TODOS"
+suffix2 <- "&fechadiar1=&fechadiar2=&comboBaseTematica=&indexcombobasetematica=0&tema=&nvocabulario=&refinar="
+
+refresh_msg <- "Haga clic aqui para ingresar nuevamente al sistema."
+nodoc_msg <- "Su búsqueda no produjo ningún documento."
+headers <- c("Accept"="text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+             "Accept-Encoding"="gzip, deflate, br",
+             "Accept-Language"="es-UY,es;q=0.9,en-US;q=0.8,en;q=0.7,es-419;q=0.6",
+             "Cache-Control"="max-age=0",
+             "Connection"="keep-alive",
+             "Cookie"="__utma=175831610.746919901.1511621124.1511621124.1511621124.1; usrts_3=anonimo; idsesionanonimo=z7lLlTGP",
+             "DNT"="1",
+             "Host"="www.impo.com.uy",
+             "Upgrade-Insecure-Requests"="1",
+             "User-Agent"="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36")
+
+type_norm_vec <- c("law"=5, "decree"=6, "resolution"=7, "rule"=11)
+
+
+# Dates vector function
+dates_url <- function(start_date, end_date, date_format) {
+  
+  date_pub_start <- start_date %>% as.Date(date_format)
+  date_pub_end <- end_date %>% as.Date(date_format)
+  
+  date_df <- as.data.frame(as.Date(seq(as.yearmon(date_pub_start), as.yearmon(date_pub_end), by=1/12), frac=1)) %>%
+    `colnames<-` ("End")
+  date_df$Start <- date_df$End-as.POSIXlt(date_df$End)$mday + 1
+  
+  return(paste0("&fechapro1=", str_replace_all(format(date_df$Start, "%d-%m-%Y"),"-","%2F"),
+                "&fechapro2=", str_replace_all(format(date_df$End, "%d-%m-%Y"),"-","%2F")))
+}
+
+
+# Base function
 request_norm_dates <- function(type, dates) {
   
   type_num <- type_norm_vec[type]
@@ -32,14 +71,14 @@ request_norm_dates <- function(type, dates) {
     
     else if (check2 == nodoc_msg) {
       print(paste0(format(month, "%m-%Y"), ": no ",
-                   tolower(names(type_num)), " found"))
+                   names(type_num), " found"))
       return(list("..", "..", "..", ".."))}
     
     # Get effective number of docs found for selected dates and request again if needed
     tryCatch({
       number_docs_effective <- (html_nodes(request_html, "#divMsg b") %>% html_text() %>% trimws())[1]
       print(paste0(format(month, "%m-%Y"),": ", number_docs_effective, " ",
-                   tolower(names(type_num)), " found"))
+                   names(type_num), " found"))
       
       if (number_docs_effective>=number_docs) {
         url <- paste0(impo_url, suffix0, number_docs_effective, "&combo1=", type_num, suffix1, x, suffix2)
@@ -60,17 +99,17 @@ request_norm_dates <- function(type, dates) {
   })
   
   return(cbind.data.frame(unlist(data[1, ]), unlist(data[2, ]), unlist(data[3, ]), 
-                          unlist(data[4, ]), names(type_num), stringsAsFactors=F) %>%
+                          unlist(data[4, ]), str_to_title(names(type_num)), stringsAsFactors=F) %>%
            `colnames<-` (c("Number", "Type2", "Title", "URL", "Type")))
 }
 
+
 # Retry function for dates that failed for unforeseen reasons
 retry_request <- function(df, type, missing) {
-  missing_dates <- missing %>% {row.names(df[., ])}
-  complete <- request_norm_dates(type, missing_dates)
   
-  if (sum(is.na(complete)) == 0) rbind.data.frame(df[!is.na(df$Number), ], 
-                                                  complete, stringsAsFactors=F)
+  complete <- request_norm_dates(type, missing)
   
+  if (sum(is.na(complete)) == 0) {
+    rbind.data.frame(df[!is.na(df$Number), ], complete, stringsAsFactors=F)}
   else {print("Failed to request every missing date, try again")}
 }
